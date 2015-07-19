2469a853396ec854b1d3643939f8e6b01a490682
Jakub Jirutka <jakub@jirutka.cz>
2015-07-19 18:34:01 +0200

    Add factory for IO objects

--- lib/asciidoctor/doctest/io.rb
+++ lib/asciidoctor/doctest/io.rb
@@ -1,3 +1,14 @@
 require 'asciidoctor/doctest/io/base_examples'
 require 'asciidoctor/doctest/io/asciidoc_examples'
 require 'asciidoctor/doctest/io/html_examples'
+require 'asciidoctor/doctest/factory'
+
+module Asciidoctor::DocTest
+  module IO
+    extend Factory
+
+    register :asciidoc, AsciidocExamples, file_ext: '.adoc'
+    register :xml, HtmlExamples, file_ext: '.xml'
+    register :html, HtmlExamples, file_ext: '.html'
+  end
+end
